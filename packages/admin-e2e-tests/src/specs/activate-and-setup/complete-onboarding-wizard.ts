c15ae54c12bed6dfa2f72068a428eab9e6edab9d
Adrian Duffell <9312929+adrianduffell@users.noreply.github.com>
2021-05-25 14:21:32 +0800

    Add state reset before obw test

--- packages/admin-e2e-tests/src/specs/activate-and-setup/complete-onboarding-wizard.ts
+++ packages/admin-e2e-tests/src/specs/activate-and-setup/complete-onboarding-wizard.ts
@@ -4,6 +4,7 @@
 import { OnboardingWizard } from '../../pages/OnboardingWizard';
 import { WcHomescreen } from '../../pages/WcHomescreen';
 import { TaskTitles } from '../../constants/taskTitles';
+import { resetWooCommerceState } from '../../utils/actions';
 import { Login } from '../../pages/Login';
 import { WcSettings } from '../../pages/WcSettings';
 
@@ -36,6 +37,8 @@ const testAdminOnboardingWizard = () => {
 		} );
 
 		it( 'can start the profile wizard', async () => {
+			await resetWooCommerceState();
+
 			await profileWizard.navigate();
 		} );
 
